c67af2f7f1f0ea8fce9d2e10a49224062f09c7ef
kyohashi <koya.ohashi@gmail.com>
2024-05-23 00:59:06 +0900

    upload

--- hierarchical_bayes_brand_choice/MNL_homogeneous.R
+++ hierarchical_bayes_brand_choice/MNL_homogeneous.R
@@ -15,32 +15,44 @@ set.seed(42)
 
 data("margarine")
 
-# Use brand 1,2,4
-purchase_df <- margarine$choicePrice %>%
-  filter(choice %in% c(1,2,4))
+purchase_df <- margarine$choicePrice
+purchase_df[,3:12] <- scale(purchase_df[,3:12])
+# apply(purchase_df[, 3:12], 2, mean)
+# apply(purchase_df[, 3:12], 2, sd)
 
-for(i in 1:10)
-  purchase_df[purchase_df['choice']==i, 'price'] <- log(purchase_df[purchase_df['choice']==i, i+2])
+P1 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,3))
+P2 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,4))
+P3 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,5))
+P4 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,6))
+P5 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,7))
+P6 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,8))
+P7 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,9))
+P8 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,10))
+P9 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,11))
+P10 <-  data.frame(intercept = rep(1, nrow(purchase_df))) %>% bind_cols(select(purchase_df,12))
 
-purchase_df <- purchase_df　%>% select(c(hhid, choice, price))
-purchase_df$choice[purchase_df$choice == 4] <- 3
-table(purchase_df$choice)
 
 ## parameters for Stan
 N <- nrow(purchase_df)
-K <- 3
-D <- 2
-X <- purchase_df %>% select(-c(hhid, choice))
-X <- data.frame(intercept = rep(1, nrow(X))) %>% 
-  bind_cols(X)
-Y <- as.integer(purchase_df$choice)
+K <- 10
+D <- ncol(P1)
+y <- as.integer(purchase_df$choice)
 
 data <- list(
   N = N,
   K = K,
   D = D,
-  X = X,
-  Y = Y
+  P1 = P1,
+  P2 = P2,
+  P3 = P3,
+  P4 = P4,
+  P5 = P5,
+  P6 = P6,
+  P7 = P7,
+  P8 = P8,
+  P9 = P9,
+  P10 = P10,
+  Y = y
 )
 
 
@@ -63,11 +75,36 @@ fit <- model$sample(
 
 # Evaluation --------------------------------------------------------------
 
-fit %>% bayesplot::rhat() %>% hist
+# fit %>% bayesplot::rhat() %>% hist(main='Hist. of Rhat')
 fit$summary('beta')
 fit$draws("beta") %>% mcmc_trace
 fit$draws('lp__', inc_warmup = FALSE) %>% mcmc_trace(n_warmup = burn)
 
-stanfit <- rstan::read_stan_csv(fit$output_files())
-log_lik <- extract_log_lik(stanfit)
-waic(log_lik)
+# stanfit <- rstan::read_stan_csv(fit$output_files())
+# log_lik <- extract_log_lik(stanfit)
+# waic(log_lik)
+
+purchase_df %>% 
+  mutate(buy = ifelse(choice == 5, 1, 0)) %>% 
+  select(c(buy, PGen_Stk)) %>% 
+  ggplot(aes(x = factor(buy), y = PGen_Stk)) +
+  geom_violin() +
+  labs(x = "buy", y = "PGen_Stk")
+
+purchase_df %>% 
+  mutate(buy = ifelse(choice == 2, 1, 0)) %>% 
+  select(c(buy, PBB_Stk)) %>% 
+  ggplot(aes(x = factor(buy), y = PBB_Stk)) +
+  geom_violin() +
+  labs(x = "buy", y = "PBB_Stk")
+
+hhid_purchase <- purchase_df %>% 
+  filter(choice == 5) %>% 
+  select(hhid)
+
+margarine$demos %>% 
+  mutate(buy = ifelse(hhid %in% hhid_purchase$hhid, 1, 0)) %>% 
+  select(c(buy, Income)) %>% 
+  ggplot(aes(x=factor(buy), y=Income)) +
+  geom_boxplot() +
+  labs(x = "buy", y="Income")
--- hierarchical_bayes_brand_choice/stan/MNL_homogeneous.stan
+++ hierarchical_bayes_brand_choice/stan/MNL_homogeneous.stan
@@ -2,25 +2,39 @@ data{
   int N; // purchase record size
   int D; // num of covariates
   int K; // brand size
-  matrix[N, D] X; // design matrix
+  matrix[N, D] P1; // design matrix
+  matrix[N, D] P2; // design matrix
+  matrix[N, D] P3; // design matrix
+  matrix[N, D] P4; // design matrix
+  matrix[N, D] P5; // design matrix
+  matrix[N, D] P6; // design matrix
+  matrix[N, D] P7; // design matrix
+  matrix[N, D] P8; // design matrix
+  matrix[N, D] P9; // design matrix
+  matrix[N, D] P10; // design matrix
   array[N] int<lower=1, upper=K> Y; // brand choice logs
 }
 
-transformed data{
-  vector[D] Zeros;
-  Zeros = rep_vector(0, D);
-}
-
 parameters {
-  matrix[D, K-1] beta_raw; // regression coefficients
+  matrix[D, K] beta_raw; // regression coefficients
 }
 
 transformed parameters {
   matrix[D, K] beta;
   matrix[N, K] utility;
   
-  beta = append_col(Zeros, beta_raw);
-  utility = X * beta;
+  beta = beta_raw;
+  beta[1,1] = 0;
+  utility[,1] = P1 * beta[,1];
+  utility[,2] = P2 * beta[,2];
+  utility[,3] = P3 * beta[,3];
+  utility[,4] = P1 * beta[,4];
+  utility[,5] = P2 * beta[,5];
+  utility[,6] = P3 * beta[,6];
+  utility[,7] = P1 * beta[,7];
+  utility[,8] = P2 * beta[,8];
+  utility[,9] = P3 * beta[,9];
+  utility[,10] = P3 * beta[,10];
 }
 
 model {
